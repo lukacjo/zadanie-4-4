c5cfe21c2193fdf07e7a773cd8018c4d91059758
Lukasz Iwanski <lukasziwanski@outlook.com>
2021-07-08 23:50:10 +0200

    zrobienie wszystkich działań oraz pozbycie sie wszystkich polskich znaków

--- 4-4.py
+++ 4-4.py
@@ -20,14 +20,22 @@ def calculator(a,b,c):
 """
 
 
-action = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")
-num1= int(input("Podaj składnik 1."))
-num2= int(input("podaj składnik 2."))
+action = input("Podaj dzialanie, poslugujac się odpowiednia liczba: 1 Dodawanie, 2 Odejmowanie, 3 Mnozenie, 4 Dzielenie: ")
+num1= int(input("Podaj skladnik 1."))
+num2= int(input("Podaj skladnik 2."))
 
 if action == '1':
-
     print(f"Dodaje {num1} i {num2}\n"\
     "wynik to ",num1+num2)
+elif action == '2':
+    print(f"Odejmuje {num2} od {num1}\n"\
+    "wynik to ",num1-num2)
+elif action == '3':
+    print(f"Mnoze {num1} i {num2}\n"\
+    "wynik to ",num1*num2)    
+elif action == '4':
+    print(f"Dziele {num1} przez {num2}\n"\
+    "wynik to ",num1/num2)
 
 #    print(num1, "+", num2, "=", num1+num2)
 #    logging.debug("dodaje %s i " % sys.argv[0])
