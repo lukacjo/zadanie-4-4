5c16fa31b3e6ec9a9e20584b96cafb5ecfac19e1
Lukasz Iwanski <lukasziwanski@outlook.com>
2021-07-09 00:31:58 +0200

    dodalem sprawdzenie czy skladniki sa liczbami przez funkcje .isdigit

--- 4-4.py
+++ 4-4.py
@@ -20,22 +20,33 @@ def calculator(a,b,c):
 """
 
 
-action = input("Podaj dzialanie, poslugujac się odpowiednia liczba: 1 Dodawanie, 2 Odejmowanie, 3 Mnozenie, 4 Dzielenie: ")
-num1= int(input("Podaj skladnik 1."))
-num2= int(input("Podaj skladnik 2."))
+action = input("Podaj dzialanie, poslugujac sie odpowiednia liczba: 1 Dodawanie, 2 Odejmowanie, 3 Mnozenie, 4 Dzielenie: ")
+num1= input("Podaj skladnik 1.")
+num2= input("Podaj skladnik 2.")
 
-if action == '1':
-    print(f"Dodaje {num1} i {num2}\n"\
-    "wynik to ",num1+num2)
-elif action == '2':
-    print(f"Odejmuje {num2} od {num1}\n"\
-    "wynik to ",num1-num2)
-elif action == '3':
-    print(f"Mnoze {num1} i {num2}\n"\
-    "wynik to ",num1*num2)    
-elif action == '4':
-    print(f"Dziele {num1} przez {num2}\n"\
-    "wynik to ",num1/num2)
+if num1.isdigit():
+    if num2.isdigit():
+        num1=int(num1)
+        num2=int(num2)
+        if action == '1':
+            print(f"Dodaje {num1} i {num2}\n"\
+            "wynik to ",num1+num2)
+        elif action == '2':
+            print(f"Odejmuje {num2} od {num1}\n"\
+            "wynik to ",num1-num2)
+        elif action == '3':
+            print(f"Mnoze {num1} i {num2}\n"\
+            "wynik to ",num1*num2)    
+        elif action == '4':
+            print(f"Dziele {num1} przez {num2}\n"\
+            "wynik to ",num1/num2)
+    else:
+        print("Nalezy podac liczbe")
+        exit(1)
+else:
+    print("Nalezy podac liczbe")
+    exit(1)
+
 
 #    print(num1, "+", num2, "=", num1+num2)
 #    logging.debug("dodaje %s i " % sys.argv[0])
